c2742e4637bd004d9bf092c72ccc38308dd79d47
Jan Löwenstrom <jan.loewenstrom@web.de>
2021-03-04 19:29:31 +0100

    update deploy script

--- camunda-money-process/deploy.sh
+++ camunda-money-process/deploy.sh
@@ -1,5 +1,4 @@
 #bin/bash
 
-/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin/java -Dmaven.multiModuleProjectDirectory=/Users/jan/Projects/camunda-student-test "-Dmaven.home=/Applications/IntelliJ IDEA.app/Contents/plugins/maven/lib/maven3" "-Dclassworlds.conf=/Applications/IntelliJ IDEA.app/Contents/plugins/maven/lib/maven3/bin/m2.conf" "-Dmaven.ext.class.path=/Applications/IntelliJ IDEA.app/Contents/plugins/maven/lib/maven-event-listener.jar" "-javaagent:/Applications/IntelliJ IDEA.app/Contents/lib/idea_rt.jar=51825:/Applications/IntelliJ IDEA.app/Contents/bin" -Dfile.encoding=UTF-8 -classpath "/Applications/IntelliJ IDEA.app/Contents/plugins/maven/lib/maven3/boot/plexus-classworlds-2.6.0.jar" org.codehaus.classworlds.Launcher -Didea.version2019.2.4 package
-
-docker cp ./target/camunda-student-test.war camunda:/camunda/webapps/
+mvn clean install
+docker cp ./target/camunda-money-process.war camunda:/camunda/webapps/
